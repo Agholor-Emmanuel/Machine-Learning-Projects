9c4ba5d601b2eb10c1158dd1c1e2819df98dcc34
AGHOLOR EMMANUEL <79978691+Agholor-Emmanuel@users.noreply.github.com>
2021-10-19 16:38:56 +0100

    Add files via upload

---
+++ Customer Cluster Analysis using K-means clustering/Customer Cluster Analysis.py
@@ -0,0 +1,61 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Mar 12 13:15:54 2021
+
+@author: EMMANUEL
+"""
+
+#Import Libraries
+
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+
+
+#import dataset and create variable
+
+dataset = pd.read_csv('Shopping_center.csv')
+X = dataset.iloc[:,3:5].values
+
+
+#Using Elbow method to find optimal value of K
+
+from sklearn.cluster import KMeans
+WCSS = []
+for i in range(1,11):
+    kmeans = KMeans(n_clusters=i, init='k-means++', n_init=10, max_iter=300, random_state=0)
+    kmeans.fit(X)
+    WCSS.append(kmeans.inertia_)
+    
+
+#Visualize optimal K  
+  
+plt.plot(range(1,11), WCSS)
+plt.title('Elbow method')
+plt.xlabel('number of cluster')
+plt.ylabel('WCSS')
+plt.show() 
+
+
+#fit k means to dataset  
+
+kmeans = KMeans(n_clusters=5, init='k-means++', n_init=10, max_iter=300, random_state=0)
+Y_kmeans = kmeans.fit_predict(X)
+
+
+#Visualize
+
+plt.scatter(X[Y_kmeans==0,0], X[Y_kmeans==0,1], s=100, color='red', label='cluster 1' )
+plt.scatter(X[Y_kmeans==1,0], X[Y_kmeans==1,1], s=100, color='blue', label='cluster 1' )
+plt.scatter(X[Y_kmeans==2,0], X[Y_kmeans==2,1], s=100, color='green', label='cluster 1' )
+plt.scatter(X[Y_kmeans==3,0], X[Y_kmeans==3,1], s=100, color='orange', label='cluster 1' )
+plt.scatter(X[Y_kmeans==4,0], X[Y_kmeans==4,1], s=100, color='purple', label='cluster 1' )
+plt.scatter(kmeans.cluster_centers_[:,0],kmeans.cluster_centers_[:,1],s=300,color='yellow', label='center')
+plt.title('Cluster of customers')
+plt.xlabel('Annual Income')
+plt.ylabel('Spending points')
+plt.legend()
+plt.show()
+
+    
+    
